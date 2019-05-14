7caf66de74899649a9e72a979e57bc126fd54fc5
danibarstad <blah@blahblah.com>
2019-05-14 00:51:34 -0500

    Trouble writing (and reading text file).

--- src/main/java/ReportGUI.java
+++ src/main/java/ReportGUI.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class ReportGUI extends JFrame{
     private JPanel mainPanel;
@@ -8,9 +9,11 @@ public class ReportGUI extends JFrame{
     private JButton addButton;
 
     private CharacterGUI characterGUI;
+    private Controller controller;
 
     ReportGUI(CharacterGUI characterGUI) {
         this.characterGUI = characterGUI;
+        this.controller = controller;
 
         setContentPane(mainPanel);
         setTitle("New Game");
@@ -21,11 +24,13 @@ public class ReportGUI extends JFrame{
 
         addButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e) throws IOException {
 
                 String gameName = newGame.getText();
                 characterGUI.GAME_ARRAY.add(gameName);
                 characterGUI.fetchCombo();
+
+                controller.writing(gameName);
 
                 characterGUI.setEnabled(true);
                 ReportGUI.this.dispose();
